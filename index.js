841b55e2da063c487f2ae88f0b5a8ea5da195675
JaoodxD <jaood96@gmail.com>
2022-07-01 12:41:45 +0300

    prettify

--- index.js
+++ index.js
@@ -5,11 +5,22 @@ let wait = (ms) => {
 }
 (async () => {
     let queue = new AwaitableQueue();
-    queue.add(() => 1).then(res => console.log(res));
-    queue.add(async () => { await wait(1000); return 2 }).then(res => console.log(res));
-    queue.add(() => 3).then(res => console.log(res)).catch(err => console.error(`error: ${err}`));
-    queue.add(async () => { await wait(1000); return 4 }).then(res => {
-        console.log(res);
-        queue.add(() => 'test').then(res => console.log(res));
-    });
+
+    queue.add(() => 1)
+        .then(res => console.log(res));
+
+
+    queue.add(async () => { await wait(1000); return 2 })
+        .then(res => console.log(res));
+
+    queue.add(() => { throw new Error('3 with error') })
+        .then(res => console.log(res))
+        .catch(err => console.error(err));
+        
+    queue.add(async () => { await wait(1000); return 4 })
+        .then(res => {
+            console.log(res);
+            queue.add(() => 'test')
+                .then(res => console.log(res));
+        });
 })();
