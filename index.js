05d3571b9ccb90ade11b6d45bc0ee758fbd0346c
Maksym Shenderuk <39469154+JaoodxD@users.noreply.github.com>
2023-03-14 04:00:57 +0300

    fix prettify

--- index.js
+++ index.js
@@ -2,26 +2,24 @@
 
 import AwaitableQueue from "./awaitable-queue.js";
 
-const wait = (ms) => {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-}
+const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 const queue = new AwaitableQueue();
 
 queue.add(() => 1)
-    .then(res => console.log(res));
+    .then((res) => console.log(res));
 
 
 queue.add(async () => { await wait(1000); return 2 })
-    .then(res => console.log(res));
+    .then((res) => console.log(res));
 
 queue.add(() => { throw new Error('3 with error') })
-    .then(res => console.log(res))
-    .catch(err => console.error(err));
+    .then((res) => console.log(res))
+    .catch((err) => console.error(err));
 
 queue.add(async () => { await wait(1000); return 4 })
-    .then(res => {
+    .then((res) => {
         console.log(res);
         queue.add(() => 'test')
-            .then(res => console.log(res));
+            .then((res) => console.log(res));
     });
